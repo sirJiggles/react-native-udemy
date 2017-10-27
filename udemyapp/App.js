5a17ea66ad429cec19b80e72d6c230db85913c14
Gareth Fuller <gareth.fuller@clark.de>
2017-10-27 23:44:12 +0200

    simple fetch for albulm data

--- udemyapp/App.js
+++ udemyapp/App.js
@@ -3,8 +3,6 @@ import { View } from 'react-native'
 import Header from './src/components/Header'
 import AlbumList from './src/components/AlbumList'
 
-// Our API end point for the albums: http://rallycoding.herokuapp.com/api/music_albums
-
 const App = () => {
   return (
     <View>
--- udemyapp/src/components/AlbumList.js
+++ udemyapp/src/components/AlbumList.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class AlbumList extends Component {
+  // Lifecycle hooks
+  componentWillMount() {
+    fetch('http://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then((json) => {
+        console.log('------------------------------------');
+        console.log(json);
+        console.log('------------------------------------');
+      })
+  }
+
   render() {
     return (
       <View>
