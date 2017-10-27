bbe978fa75fe831b5da3b829effe045ff18d6ae4
Gareth Fuller <gareth.fuller@clark.de>
2017-10-27 23:28:30 +0200

    set up our album list component, now wil make ajax request for the data

--- udemyapp/App.js
+++ udemyapp/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { View } from 'react-native'
 import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
+
+// Our API end point for the albums: http://rallycoding.herokuapp.com/api/music_albums
 
 const App = () => {
   return (
     <View>
-      <Header headerText='Gareth rocks' />
+      <Header headerText='Albums' />
+      <AlbumList>
+
+
+      </AlbumList>
     </View>
   )
 }
