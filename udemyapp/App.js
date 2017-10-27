0361be31038e5bb0bb1d690a674c03a612b4f9a4
Gareth Fuller <gareth.fuller@clark.de>
2017-10-27 23:07:21 +0200

    adding some styles for the header

--- udemyapp/App.js
+++ udemyapp/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import Header from './src/components/Header'
 
 const App = () => {
   return (
     <View>
       <Header />
-      <Text>Some text</Text>
     </View>
   )
 }
--- udemyapp/src/components/Header.js
+++ udemyapp/src/components/Header.js
@@ -1,8 +1,30 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 
 const Header = () => {
-  return <Text>Albums!</Text>
+  const { textStyle, viewStyle } = styles
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15,
+    height: 60,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
+  }
+})
 
 export default Header
