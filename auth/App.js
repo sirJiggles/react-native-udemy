7ee14b78c97b1bd60532b6c5a69c2779798051b6
Gareth Fuller <gareth.fuller@clark.de>
2017-10-29 18:29:58 +0100

    linting

--- auth/App.js
+++ auth/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Header } from './src/components/common'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Header headerText='React native rocks!'/>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-
-  },
-});
