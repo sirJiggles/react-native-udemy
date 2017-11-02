a9072a37b9729de7a2111cae9ac8e3aafc8502f7
Gareth Fuller <gareth.fuller@clark.de>
2017-11-02 21:51:46 +0100

    hook up being logged in and out!

--- auth/App.js
+++ auth/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
 import { View } from 'react-native';
-import { Header } from './src/components/common'
+import { Header, Button, Spinner } from './src/components/common'
 import LoginForm from './src/components/LoginForm'
 
 export default class App extends Component {
+  state = {
+    // not sure if logged in first?
+    loggedIn: null
+  }
+
   componentWillMount() {
     // set up firebase using our config
     firebase.initializeApp({
@@ -15,13 +20,38 @@ export default class App extends Component {
       storageBucket: 'reactnativeauth-77fad.appspot.com',
       messagingSenderId: '999479817949'
     })
+
+    // singed in and out events call out function
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{height: 45}}>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log out
+            </Button>
+          </View>)
+      case false:
+        return <LoginForm />
+      default:
+        return <Spinner size='large' />
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='React native rocks!'/>
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
