ab3f3fbcd030b76a1cb87277122baf400bafaa89
Gareth Fuller <gareth.fuller@clark.de>
2017-11-12 22:03:50 +0100

    initial project setup

---
+++ tech-stack/App.js
@@ -0,0 +1,15 @@
+import { AppRegistry } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )
+}
+
+export default App
+
+AppRegistry.registerComponent('tech_stack', App)
