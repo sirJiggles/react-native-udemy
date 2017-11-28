0d3ac62771729e03c4176e2f03a390b255166ad6
Gareth Fuller <gareth.fuller@clark.de>
2017-11-28 22:39:39 +0100

    port common

---
+++ manager/src/components/common/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Text, StyleSheet, View } from 'react-native'
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  )
+}
+
+Header.propTypes = {
+  headerText: PropTypes.string
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15,
+    height: 60,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+})
+
+export { Header }
