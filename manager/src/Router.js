ebba8eb79e7d34a3d4df3c6dd8ff45beb3506c56
Gareth Fuller <gareth.fuller@clark.de>
2017-12-06 23:33:01 +0100

    navigating from employee list to employee ceate

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,8 +1,9 @@
 // one location where we can define all routes
 import React from 'react'
-import { Scene, Router, Stack } from 'react-native-router-flux'
+import { Scene, Router, Stack, Actions } from 'react-native-router-flux'
 import LoginFrom from './components/LoginForm'
 import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => {
   return (
@@ -16,13 +17,21 @@ const RouterComponent = () => {
             initial
           />
         </Scene>
-        <Scene key='main'>
+        <Stack key='main'>
           <Scene
             key='employeeList'
             component={EmployeeList}
             title='Employees'
+            rightTitle='Add'
+            onRight={() => Actions.employeeCreate()}
+            initial
           />
-        </Scene>
+          <Scene
+            key='employeeCreate'
+            component={EmployeeCreate}
+            title='Create Employee'
+          />
+        </Stack>
       </Scene>
     </Router>
   )
