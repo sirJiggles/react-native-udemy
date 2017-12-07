2ff567a818c556628837ef435d39f18486e8297c
Gareth Fuller <gareth.fuller@clark.de>
2017-12-07 21:29:33 +0100

    add in the shift picker

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import { Picker, StyleSheet } from 'react-native'
 import { Card, CardSection, InputArea, Button } from './common'
 import { connect } from 'react-redux'
 import { employeeUpdate } from '../actions'
 
 class EmployeeCreate extends Component {
+
+  // helper for our picker
+  daysOfWeek() {
+    return [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday'
+    ]
+  }
+
   render() {
     return (
       <Card>
@@ -11,8 +26,8 @@ class EmployeeCreate extends Component {
           <InputArea
             label='name'
             placeholder='jane'
-            value={this.props.name}
-            onChangeText={value => this.props.employeeUpdate({prop:'name', value})}
+            value={ this.props.name }
+            onChangeText={ value => this.props.employeeUpdate({prop:'name', value}) }
           />
         </CardSection>
 
@@ -20,13 +35,19 @@ class EmployeeCreate extends Component {
           <InputArea
             label='phone'
             placeholder='55-555-555'
-            value={this.props.phone}
-            onChangeText={value => this.props.employeeUpdate({prop:'phone', value})}
+            value={ this.props.phone }
+            onChangeText={ value => this.props.employeeUpdate({prop:'phone', value}) }
           />
         </CardSection>
 
         <CardSection>
-
+          <Picker
+            style={ styles.picker }
+            selectedValue={ this.props.shift }
+            onValueChange={ value => this.props.employeeUpdate({prop: 'shift', value}) }
+          >
+            { this.daysOfWeek().map(day => <Picker.Item key={day} label={day} value={day} />) }
+          </Picker>
         </CardSection>
 
         <CardSection>
@@ -44,5 +65,11 @@ const mapStatesToProps = (state) => {
 
   return { name, phone, shift }
 }
+
+const styles = StyleSheet.create({
+  picker: {
+    flex: 1
+  }
+})
 
 export default connect(mapStatesToProps, { employeeUpdate })(EmployeeCreate)
